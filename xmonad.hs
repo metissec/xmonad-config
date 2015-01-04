ec69f6381f8248b4fb2e16b25afdaeed7a496d17
metissec <jrp494@gmail.com>
2015-01-04 14:36:24 +0000

    added f1 and f2 function for chromebook layout

--- xmonad.hs
+++ xmonad.hs
@@ -3,8 +3,9 @@ import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
-import XMonad.Actions.SpawnOn
+import XMonad.Actions.CycleWS
 import System.IO
+
 
 main = do
     xmproc <- spawnPipe "xmobar"
@@ -16,16 +17,16 @@ main = do
         , layoutHook = avoidStruts  $  layoutHook defaultConfig
         , logHook = dynamicLogWithPP xmobarPP
                         { ppOutput = hPutStrLn xmproc
-                        , ppTitle = xmobarColor "green" "" . shorten 50
+                        , ppTitle = xmobarColor "green" "" . shorten 25
                         }
-	,startupHook = do
-		     spawnOn "1" "urxvt"
 	} `additionalKeys`
 	[((mod1Mask, xK_y), spawn "chromium")
 	,((mod1Mask, xK_u), spawn "emacs")
+	,((0       , xK_F1), prevWS)
+   	,((0       , xK_F2), nextWS)
 	,((0       , xK_F6), spawn "xbacklight -dec 10")
 	,((0       , xK_F7), spawn "xbacklight -inc 10")
 	,((0       , xK_F8), spawn "amixer set Master toggle")
 	,((0       , xK_F9), spawn "amixer set Master 10- unmute")
-	,((0       , xK_F10), spawn "amixer set Master 10+ unmute")
+	,((0       , xK_F10),spawn "amixer set Master 10+ unmute")
 	]
