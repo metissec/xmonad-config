e276d44790df660add19bea042640c0a6ff49bc0
metissec <jrp494@gmail.com>
2015-01-05 17:15:30 +0000

    added

--- xmonad.hs
+++ xmonad.hs
@@ -4,6 +4,7 @@ import XMonad.Hooks.ManageDocks
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
 import XMonad.Actions.CycleWS
+import qualified XMonad.StackSet as W
 import System.IO
 
 
@@ -23,13 +24,15 @@ main = do
 	} `additionalKeys`
 	[((myModMask, xK_y), spawn "chromium")
 	,((myModMask, xK_u), spawn "emacs")
-	,((0       , xK_F1), prevWS)
-   	,((0       , xK_F2), nextWS)
-	,((0       , xK_F6), spawn "xbacklight -dec 10")
-	,((0       , xK_F7), spawn "xbacklight -inc 10")
-	,((0       , xK_F8), spawn "amixer set Master toggle")
-	,((0       , xK_F9), spawn "amixer set Master 10- unmute")
-	,((0       , xK_F10),spawn "amixer set Master 10+ unmute")
+	,((mod1Mask , xK_F1), windows W.focusUp)
+	,((mod1Mask , xK_F2), windows W.focusDown)
+	,((0        , xK_F1), prevWS)
+   	,((0        , xK_F2), nextWS)
+	,((0        , xK_F6), spawn "xbacklight -dec 10")
+	,((0        , xK_F7), spawn "xbacklight -inc 10")
+	,((0        , xK_F8), spawn "amixer set Master toggle")
+	,((0        , xK_F9), spawn "amixer set Master 10- unmute")
+	,((0        , xK_F10),spawn "amixer set Master 10+ unmute")
 	]
 	
 myModMask = mod4Mask
